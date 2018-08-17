e0f6074665d00d59e54cfea64fff8f380efec2ba
Gerardo Paredes <gerardoparedes@Gerardos-MacBook-Pro.local>
2018-08-17 00:40:37 -0700

    setting up mongo db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,14 +3,7 @@ let mongoose = require('mongoose'); //importing mongoose module
 mongoose.Promise = global.Promise; //setting up mongoose to be used with promises
 // mongoose.connect('mongodb://localhost:27017/TodoApp'); //setting up the mongoDB
 
-let dbUser = 'admin';
-let dbPassword = 'Kief323812!';
-
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: `mongodb://${dbUser}:${dbPassword}!@ds123562.mlab.com:23562/todoapp`
-  };
-  mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+mongoose.connect( process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
