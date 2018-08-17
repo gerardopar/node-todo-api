bb86ec0d3f97a0ac40a022b7094362778d08cf33
Gerardo Paredes <gerardoparedes@Gerardos-MacBook-Pro.local>
2018-08-17 10:36:28 -0700

    mlab setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,11 @@ let mongoose = require('mongoose'); //importing mongoose module
 mongoose.Promise = global.Promise; //setting up mongoose to be used with promises
 // mongoose.connect('mongodb://localhost:27017/TodoApp'); //setting up the mongoDB
 
-mongoose.connect( process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp');
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://admin:Kief323812!@ds123562.mlab.com:23562/todoapp'
+  };
+  mongoose.connect( process.env.PORT ? db.localhost : db.mlab);
 
 module.exports = {
     mongoose
